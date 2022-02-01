aa4f77cba22865a04bd2a7393ae774ea4ea5488a
Sarah <sarahosmartins@gmail.com>
2022-01-31 22:54:28 -0300

    pagina login sarah

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Modal from "react-modal";
 import { Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./assets/styles/global";
 import { Home } from "./pages/home/Home/index";
+import LoginForm from "./pages/login/Loginform";
 
 Modal.setAppElement("#root");
 
@@ -10,6 +11,7 @@ export function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="login" element={<LoginForm />} />
       </Routes>
       <GlobalStyle />
     </>
