1d619888aada8e97e45e73bf40a1fa332b74f3c4
ravena <ravenascampos@gmail.com>
2022-02-07 04:48:47 -0400

    alterações merge

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,8 @@ import { Home } from "./pages/home/Home/index";
 import Profile from "./pages/profile/Profile";
 import NavBar from "./components/Navbar";
 import Cart from "./pages/cart/Cart";
-<<<<<<< HEAD
 import Log from "./pages/login/Log";
 import Modal from "react-modal";
-import { Routes, Route } from "react-router-dom";
-=======
-import Modal from "react-modal";
->>>>>>> Developer
 
 Modal.setAppElement("#root");
 export function App() {
@@ -21,10 +16,7 @@ export function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/cart" element={<Cart />} />
-<<<<<<< HEAD
         <Route path="login" element={<Log />} />
-=======
->>>>>>> Developer
       </Routes>
       <GlobalStyle />
     </>
